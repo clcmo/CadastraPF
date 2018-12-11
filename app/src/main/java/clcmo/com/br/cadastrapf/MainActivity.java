d8c7681f8372ec5d933be27a7175fed99752ca1c
Camila L <camila.leite.oliveira@gmail.com>
2018-12-11 17:20:56 -0200

    set AndroidManifest, MainActivity and Signup

--- app/src/main/java/clcmo/com/br/cadastrapf/MainActivity.java
+++ app/src/main/java/clcmo/com/br/cadastrapf/MainActivity.java
@@ -1,13 +1,29 @@
 package clcmo.com.br.cadastrapf;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+    private Button gotoSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createbutton();
+    }
+
+    private void createbutton() {
+        gotoSignup = (Button) findViewById(R.id.gotoSignup);
+        gotoSignup.setOnClickListener((OnClickListener) this);
+    }
+
+    public void onClick (View v){
+        Intent intent = new Intent(MainActivity.this, Signup.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/clcmo/com/br/cadastrapf/Signup.java
+++ app/src/main/java/clcmo/com/br/cadastrapf/Signup.java
@@ -1,13 +1,43 @@
 package clcmo.com.br.cadastrapf;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Signup extends AppCompatActivity {
+    private Button su_buttonback, su_buttonList, su_buttonSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        createBackbutton();
+        createSavebutton();
+        createListbutton();
+
     }
+
+    private void createBackbutton() {
+        su_buttonback = (Button) findViewById(R.id.su_buttonback);
+        su_buttonback.setOnClickListener((View.OnClickListener) this);
+    }
+
+    private void createSavebutton() {
+        su_buttonSave = (Button) findViewById(R.id.su_buttonSave);
+        su_buttonSave.setOnClickListener((View.OnClickListener) this);
+    }
+
+    private void createListbutton() {
+        su_buttonList = (Button) findViewById(R.id.gotoSignup);
+        su_buttonList.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void onClick (View v){
+        Intent intent = new Intent(Signup.this, List.class);
+        startActivity(intent);
+    }
+
+
 }
