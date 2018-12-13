4aa25d7cd85231444fb842a83b833ba92d9a90f8
Camila L <camila.leite.oliveira@gmail.com>
2018-12-13 19:55:07 -0200

    update classes and set activites

--- app/src/main/java/clcmo/com/br/cadastrapf/MainActivity.java
+++ app/src/main/java/clcmo/com/br/cadastrapf/MainActivity.java
@@ -4,27 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+/*import android.view.View.OnClickListener;
+import android.widget.Button;*/
 
-public class MainActivity extends AppCompatActivity implements OnClickListener {
-    private Button gotoSignup;
+public class MainActivity extends AppCompatActivity {
+    //private Button gotoSignup;
+    private View v;
     private Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gotoButton();
+        gotoButton(v);
     }
 
-    private void gotoButton() {
-        gotoSignup = (Button) findViewById(R.id.gotoSignup);
-        gotoSignup.setOnClickListener((OnClickListener) this);
-    }
-
-    public void onClick (View v){
-        i = new Intent(MainActivity.this, Signup.class);
+    public void gotoButton(View v) {
+        i = new Intent(this, Signup.class);
         startActivity(i);
     }
 }
--- app/src/main/java/clcmo/com/br/cadastrapf/Signup.java
+++ app/src/main/java/clcmo/com/br/cadastrapf/Signup.java
@@ -4,51 +4,63 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+//import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class Signup extends AppCompatActivity {
-    private Button su_buttonback, su_buttonList, su_buttonSave;
+    //private Button su_buttonback, su_buttonList, su_buttonSave;
+    private EditText et_su_name, et_su_nd, et_su_age, et_su_phone, et_su_mail;
+    private String name, nd, age, phone, mail;
+    private ListView list_users;
+    //private List<String> users;
     private Intent i;
+    public static final String EXTRA_MESSAGE = "br.com.clcmo.cadastrapf.List";
+    private View v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        Backbutton();
-        Savebutton();
-        Listbutton();
+        BackButton(v);
+        SaveButton(v);
+        ListButton(v);
     }
 
-    private void Backbutton() {
-        su_buttonback = (Button) findViewById(R.id.su_buttonback);
-        su_buttonback.setOnClickListener((View.OnClickListener) this);
-    }
-
-    private void Savebutton() {
-        su_buttonSave = (Button) findViewById(R.id.su_buttonSave);
-        su_buttonSave.setOnClickListener((View.OnClickListener) this);
-    }
-
-    private void Listbutton() {
-        su_buttonList = (Button) findViewById(R.id.su_buttonList);
-        su_buttonList.setOnClickListener((View.OnClickListener) this);
-    }
-
-    public void onClickList (View v){
-        //aqui, ele só clica para a acessar a lista e nada mais
-        i = new Intent(Signup.this, List.class);
-        startActivity(i);
-    }
-
-    public void onClickSave (View v){
-        //preciso acrescentar aqui as informações para a List.class
-        i = new Intent(Signup.this, List.class);
-        startActivity(i);
-    }
-
-    public void onClickBack (View v){
+    public void BackButton(View v) {
         //aqui, é apenas o retorno a página inicial
-        i = new Intent(Signup.this, MainActivity.class);
+        i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void SaveButton(View v){
+        //preciso acrescentar aqui as informações para a List.class
+        i = new Intent(this, List.class);
+
+        et_su_name = (EditText) findViewById(R.id.et_su_name);
+        et_su_nd = (EditText) findViewById(R.id.et_su_nd);
+        et_su_age = (EditText) findViewById(R.id.et_su_age);
+        et_su_phone = (EditText) findViewById(R.id.et_su_phone);
+        et_su_mail = (EditText) findViewById(R.id.et_su_mail);
+
+        name  = et_su_name.getText().toString();
+        nd  = et_su_nd.getText().toString();
+        age  = et_su_age.getText().toString();
+        phone  = et_su_phone.getText().toString();
+        mail = et_su_mail.getText().toString();
+
+        i.putExtra(EXTRA_MESSAGE, name);
+        i.putExtra(EXTRA_MESSAGE, nd);
+        i.putExtra(EXTRA_MESSAGE, age);
+        i.putExtra(EXTRA_MESSAGE, phone);
+        i.putExtra(EXTRA_MESSAGE, mail);
+
+        startActivity(i);
+    }
+
+    public void ListButton(View v) {
+        //aqui, ele só clica para a acessar a lista e nada mais
+        i = new Intent(this, List.class);
         startActivity(i);
     }
 }
